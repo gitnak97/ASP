36329950e38ee373153de4d264654643167839a0
nakhoon <nak970129@naver.com>
2025-05-23 16:45:01 +0900

    test

--- Currency_Weather/App_Start/WebApiConfig.cs
+++ Currency_Weather/App_Start/WebApiConfig.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
-namespace Currency_Weather
+public static class WebApiConfig
 {
-    public static class WebApiConfig
+    public static void Register(HttpConfiguration config)
     {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API 구성 및 서비스
+        config.Formatters.Remove(config.Formatters.XmlFormatter);
 
-            // Web API 경로
-            config.MapHttpAttributeRoutes();
+        config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
+        config.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{id}",
+            defaults: new { id = RouteParameter.Optional }
+        );
     }
 }
--- Currency_Weather/Controllers/WeatherController.cs
+++ Currency_Weather/Controllers/WeatherController.cs
@@ -1,11 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Currency_Weather.Controllers
 {
-    public class WeatherController
+    [RoutePrefix("api/weather")]
+    public class WeatherController : ApiController
     {
+        [HttpGet]
+        [Route("")]
+        public async Task<IHttpActionResult> GetWeather()
+        {
+            try
+            {
+                string apiKey = "CphOgsk4rTQcVJG7C0QeYx0abOMQDoLM";
+                string locationKey = "318251"; // 예: 이스탄불 (터키)
+                string url = $"http://dataservice.accuweather.com/currentconditions/v1/{locationKey}?apikey={apiKey}&language=ko&details=true";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetAsync(url);
+                    var json = await response.Content.ReadAsStringAsync();
+
+                    if (!response.IsSuccessStatusCode)
+                        return Content(response.StatusCode, json);
+
+                    var data = JsonConvert.DeserializeObject<JArray>(json)[0];
+
+                    var result = new
+                    {
+                        상태 = data["WeatherText"],
+                        온도 = data["Temperature"]["Metric"]["Value"] + "°C",
+                        체감온도 = data["RealFeelTemperature"]["Metric"]["Value"] + "°C",
+                        습도 = data["RelativeHumidity"] + "%",
+                        갱신시각 = data["LocalObservationDateTime"]
+                    };
+
+                    return Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex); // 서버 오류 시 예외 출력
+            }
+        }
     }
 }
